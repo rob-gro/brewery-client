8cc298c3ff8a2aa5d49a9898cff931a41558af70
RobGro <r.grodzki74@gmail.com>
2022-04-10 12:15:21 +0100

    Initial commit

---
+++ src/main/java/com/robgro/breweryclient/web/client/package-info.java
@@ -0,0 +1,1 @@
+package com.robgro.breweryclient.web.client;
---
+++ src/test/java/com/robgro/breweryclient/BreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robgro.breweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
